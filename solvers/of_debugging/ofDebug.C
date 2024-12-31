a1ff71548e2409851ddda3b5dae09eaeecfa6fe7
aditya <adibantwal@gmail.com>
2024-12-31 15:00:04 +0400

    some new solvers

---
+++ solvers/of_debugging/ofDebug.C
@@ -0,0 +1,144 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) YEAR AUTHOR,AFFILIATION
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    ofDebug
+
+Description
+    This solver explores debugging.
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+
+scalar calculatePressure(scalar t, vector x, vector x0, scalar scale);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    Info <<"Reading in transport properties" << nl << endl;
+
+    IOdictionary transportProperties
+    {
+        IOobject
+        {
+            "transportProperties",
+            runTime.constant(),
+            mesh,
+            IOobject::MUST_READ_IF_MODIFIED,
+            IOobject::NO_WRITE
+        }
+    };
+    
+    dimensionedScalar nu
+    {
+        "nu",
+        dimViscosity, 
+        transportProperties.lookup("nu")
+    };
+
+    Info <<"Reading field p\n" <<endl;
+
+    volScalarField p
+    {
+        IOobject
+        {
+            "p",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        },
+        mesh
+    };
+
+    Info <<"Reading field U\n" <<endl;
+
+    volVectorField U
+    {
+        IOobject
+        {
+            "U",
+            runTime.timeName(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE           
+        },
+        mesh
+    };
+
+    const vector originVector(0.05, 0.05, 0.005);
+    const scalar rFarCell = max(
+        mag(dimensionedVector("x0",dimLength,originVector)-mesh.C())
+    ).value();
+
+    Info << nl << "rFarCell is: " << rFarCell << endl;
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info << "\nStarting time loop\n" << endl;
+    while (runTime.loop())
+    {
+        Info << "Time = " << runTime.timeName() << nl << endl;
+
+        for(label cellI =0; cellI < mesh.C().size(); cellI++)
+        {
+            p[cellI] = calculatePressure(runTime.time().value(),mesh.C()[cellI], originVector, rFarCell);
+        };
+
+        // if runTime.timeName() is an integer print the p at the cell in the middle of the domain. Ideally it should be zero.
+
+    if (mag( runTime.value() - round(runTime.value()) ) < 1e-6)
+    {
+        label midCell = mesh.C().size() / 2; // Index for the middle cell
+        Info << "Pressure at the middle cell (" << midCell << "): " << p[midCell] << " at time = " << runTime.value() << nl << endl;
+    };
+
+        U = fvc::grad(p)*dimensionedScalar("t",dimTime,1.0);
+        runTime.write();
+    };
+
+    Info<< nl;
+    runTime.printExecutionTime(Info);
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+// Define pressure function
+
+scalar calculatePressure(scalar t, vector x, vector x0, scalar scale)
+{
+    scalar r (mag(x-x0)/scale);
+    scalar rR (1.0/(r + 1e-12));
+    scalar f (1.0);
+
+    return Foam::sin(2.0*Foam::constant::mathematical::pi*f*t)*rR;
+}
+// ************************************************************************* //
---
+++ solvers/of_scalartransport/ofScalarTransport.C
@@ -0,0 +1,76 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) YEAR AUTHOR,AFFILIATION
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    ofScalarTransport
+
+Description
+    Exploring the transport equation through defining a passive scalar transport
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createFields.H"
+
+    solve 
+    (
+        //convection diffusion steady state
+        fvm::div(phi,T) - fvm::laplacian(DT,T)
+    );
+
+    volScalarField result
+    (
+        IOobject
+        (
+            "result",
+            runTime.timeName(), 
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        T
+    );
+    result.write();
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< nl;
+    runTime.printExecutionTime(Info);
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
